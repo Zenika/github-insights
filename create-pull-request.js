8a2b94182c14017533ad055fc0e821ceb9248f9d
Charles-Henri GUERIN <charleshenri.guerin@me.com>
2019-09-10 11:41:12 +0200

    ✨Create pull request with new insights (#10)
    * :plus_heavy_sign: Add dependency @octokit/rest
    
    * :memo: Add environement variables
    
    * GITHUB_ORGA
    * GITHUB_WEBSITE
    
    * :sparkles: Create a pull request with new insights
    
    * :sparkles: Use commit message as PR title

---
+++ create-pull-request.js
@@ -0,0 +1,85 @@
+require('dotenv').config()
+
+const Octokit = require('@octokit/rest')
+const { promisify } = require('util')
+const readFile = promisify(require('fs').readFile)
+
+const client = new Octokit({
+  auth: process.env.GITHUB_OAUTH,
+  previews: ['shadow-cat'],
+})
+
+const COMMON_PAYLOAD = {
+  owner: process.env.GITHUB_ORGA,
+  repo: process.env.GITHUB_WEBSITE,
+}
+
+const branch = `insights-${getDate()}`
+
+createBranchFromMaster(client, branch)
+  .then(() => readFile('data/stats.json', 'base64'))
+  .then(content => updateFile(
+    client,
+    'src/data/stats.json',
+    content,
+    branch,
+  ))
+  .then(({ data }) => data.commit.message)
+  .then(title => createPullRequest(client, branch, title))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
+
+function createBranchFromMaster(client, branch) {
+  return client.repos.getBranch({
+    ...COMMON_PAYLOAD,
+    branch: 'master',
+  })
+  .then(branch => branch.data.commit.sha)
+  .then(sha => {
+    return client.git.createRef({
+      ...COMMON_PAYLOAD,
+      ref: `refs/heads/${branch}`,
+      sha,
+    })
+  })
+  .catch(() => {
+    console.log(`Branch "${branch}" already exists: skip creation.`)
+  })
+}
+
+function updateFile(client, path, content, branch, date = getDate()) {
+  return client.repos.getContents({
+    ...COMMON_PAYLOAD,
+    path,
+    ref: branch,
+  })
+  .then(content => content.data)
+  .then(({ name, sha }) => {
+    const message = `:card_file_box: Update ${name} ${date}`
+
+    return client.repos.createOrUpdateFile({
+      ...COMMON_PAYLOAD,
+      branch,
+      path,
+      message,
+      content,
+      sha,
+    })
+  })
+}
+
+function createPullRequest(client, head, title) {
+  return client.pulls.create({
+    ...COMMON_PAYLOAD,
+    base: 'master',
+    draft: true,
+    head,
+    title,
+  })
+}
+
+function getDate(date = new Date()) {
+  return date.toISOString().split('T')[0]
+}
