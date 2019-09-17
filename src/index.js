5920de5da6b01dfdbe3b905295a67273beb05922
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-09-17 11:15:00 +0200

    :recycle: Extract writing file logic

--- src/index.js
+++ src/index.js
@@ -6,30 +6,23 @@
 
   const { getRateLimit, getRepositoryContributors, getRepositoriesByLogin, getMembersByOrganization } = require('./queries.js')
   const client = require('./graphql.js')
-  const { sleep } = require('./utils')
+  const { sleep, createDataFolder, writeMember, writeMembers, writeOrganization } = require('./utils')
 
   const githubId = process.env.GITHUB_ID
   const githubToken = process.env.GITHUB_OAUTH
   const githubOrganization = process.argv[2] || process.env.GITHUB_ORGA
-  const rootDir = '../data'
 
-  if (!fs.existsSync(path.join(__dirname, rootDir))) {
-    fs.mkdirSync(path.join(__dirname, rootDir), {},  (err) => {
-      if (err) throw err;
-    })
-  }
+  createDataFolder()
 
   let members
   try {
     members = await getMembersByOrganization(githubOrganization)
-    fs.writeFileSync(path.join(__dirname, rootDir, 'members.json'), JSON.stringify(members, undefined, 2))
+    writeMembers(members)
   } catch(e) {
-    console.error(e)
     console.error('Error while fetching members', JSON.stringify(e, undefined, 2))
     process.exit(0)
   }
   console.log(`Numbers of members: ${members.length}`)
-  const membersInError = []
 
   const bar = new ProgressBar('downloading [:bar] :login (:percent)', {
     complete: '=',
@@ -46,7 +39,6 @@
       repositories = await getRepositoriesByLogin(member.login, 'user')
     } catch (e) {
       console.error(e)
-      membersInError.push(member.login)
       continue
     }
 
@@ -61,16 +53,7 @@
       }
     }
 
-    fs.writeFileSync(path.join(__dirname, rootDir, `${member.login}.json`), JSON.stringify(
-      { ...member, repositories },
-      undefined,
-      2,
-    ))
-  }
-
-  if (membersInError.length) {
-    console.error(`Oups, something went wrong for some members. Please refer to ${path.join(__dirname, rootDir, 'membersInError.json')}`)
-    fs.writeFileSync(path.join(__dirname, rootDir, 'membersInError.json'), JSON.stringify(membersInError, undefined, 2))
+    writeMember({ ...member, repositories })
   }
 
   let organization = []
@@ -80,5 +63,5 @@
     console.error(`Can't retrieve organization data`)
   }
 
-  fs.writeFileSync(path.join(__dirname, rootDir, 'organization.json'), JSON.stringify(organization, undefined, 2))
+  writeOrganization(organization)
 })()
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,41 @@
+const fs = require('fs')
+const path = require('path')
+
+const rootDir = '../data'
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function prettyJson(obj) {
+  return JSON.stringify(obj, undefined, 2)
+}
+
+function createDataFolder() {
+  if (!fs.existsSync(path.join(__dirname, rootDir))) {
+    fs.mkdirSync(path.join(__dirname, rootDir), {},  (err) => {
+      if (err) throw err;
+    })
+  }
+}
+
+function writeMember(member) {
+  fs.writeFileSync(path.join(__dirname, rootDir, `${member.login}.json`), prettyJson(member))
+}
+
+function writeMembers(members) {
+  fs.writeFileSync(path.join(__dirname, rootDir, 'members.json'), prettyJson(members))
+}
+
+function writeOrganization(organization) {
+  fs.writeFileSync(path.join(__dirname, rootDir, 'organization.json'), prettyJson(organization))
+}
+
 module.exports = {
   sleep,
+  writeMember,
+  writeMembers,
+  writeOrganization,
+  createDataFolder,
 }
 
